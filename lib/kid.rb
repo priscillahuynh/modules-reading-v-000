33038a4e80ebe68f238f2f081f977b46cfe0dafb
Priscilla H <priscillamhuynh@gmail.com>
2018-07-11 05:41:53 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance 
+  include Dance
 
-  attr_accessor :name 
+  attr_accessor :name
 
   def initialize(name)
-    @name=name 
+    @name=name
   end 
 
 end
